d45248f14807aaad43a4896547e0c6e529bf5ed5
Dmoreno2903 <jaguirremo@unal.edu.co>
2023-10-22 18:18:05 -0500

    Correción del formulario y conexión con postgreSQL

--- client/src/api/users_api.js
+++ client/src/api/users_api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const userApi = axios.create({
-    baseURL: 'http://localhost:8000/api/users/',
+    /*baseURL: 'http://localhost:8000/api/users/',*/
+    baseURL: 'https://mediminder-e6ow.onrender.com/api/users/',
 });
 
 export const getAllUsers = () => userApi.get('/');
