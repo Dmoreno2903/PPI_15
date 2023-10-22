e35b04b7f0d04a94338e3d0c7e8dd33d2f0deb71
Dmoreno2903 <jaguirremo@unal.edu.co>
2023-10-22 17:47:58 -0500

    Despliegue

--- client/src/api/eps_api.js
+++ client/src/api/eps_api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const userApi = axios.create({
-    baseURL: 'http://https://mediminder-e6ow.onrender.com/api/users/:8000/api/eps/',
+    /*baseURL: 'http://localhost:8000/api/eps/',*/
+    baseURL: 'https://mediminder-e6ow.onrender.com/api/eps/',
 });
 
 export const getAllEps = () => userApi.get('/');
