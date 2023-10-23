dfe0c91ad49e45059d4f0650ddd05772830f0c94
cAos <65823540+Cesar-580@users.noreply.github.com>
2023-10-23 10:40:54 -0500

    Correccion errores base datos

--- users/urls.py
+++ users/urls.py
@@ -2,13 +2,15 @@ from rest_framework import routers
 from .api import EpsViewSet
 from .api import IpsViewSet
 from .api import UserViewSet
+from .api import PerfilUsuarioViewSet
+from .api import TriageViewSet
 
 router = routers.DefaultRouter()
 
 router.register('api/eps', EpsViewSet, 'eps')
 router.register('api/ips', IpsViewSet, "ips")
 router.register('api/user', UserViewSet, 'user')
-router.register('api/perfil-usuario', UserViewSet, 'perfil-usuario')
-router.register('api/triage', UserViewSet, 'triage')
+router.register('api/perfil-usuario', PerfilUsuarioViewSet, 'perfil-usuario')
+router.register('api/triage', TriageViewSet, 'triage')
 
 urlpatterns = router.urls
