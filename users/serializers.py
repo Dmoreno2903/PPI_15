4cdb0d1013f74dec3cfe052b63a3aff464f85c56
Dmoreno2903 <jaguirremo@unal.edu.co>
2023-10-22 19:56:52 -0500

    Se elimina la relación usuario

--- users/serializers.py
+++ users/serializers.py
@@ -1,24 +1,6 @@
 from rest_framework import serializers
-from .models import User
 from .models import Eps
 from .models import Ips
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            'name',
-            'id',
-            'contacto',
-            'email',
-            'eps',
-            'genero',
-            'usuario',
-            'password',
-            'name_emergencia',
-            'contacto_emergencia',
-        )
-        read_only_fields = ('created_at', )
 
 class EpsSerializer(serializers.ModelSerializer):
     class Meta:
