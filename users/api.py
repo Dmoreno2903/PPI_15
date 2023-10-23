795da0b0d8cf58008afffa56fe4404c710c223a3
cAos <65823540+Cesar-580@users.noreply.github.com>
2023-10-23 10:32:26 -0500

    Correccion errores

--- users/api.py
+++ users/api.py
@@ -1,10 +1,14 @@
 from .models import Eps
 from .models import Ips
 from .models import User
+from .models import PerfilUsuario
+from .models import Triage
 from rest_framework import viewsets, permissions
 from .serializers import EpsSerializer
 from .serializers import IpsSerializer
 from .serializers import UserSerializer
+from .serializers import PerfilUsuarioSerializer
+from .serializers import TriageSerializer
 
 class EpsViewSet(viewsets.ModelViewSet):
     queryset = Eps.objects.all()
@@ -20,3 +24,13 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     permissions_classes = [permissions.AllowAny]
     serializer_class = UserSerializer
+
+class PerfilUsuarioViewSet(viewsets.ModelViewSet):
+    queryset = PerfilUsuario.objects.all()
+    permissions_classes = [permissions.AllowAny]
+    serializer_class = PerfilUsuarioSerializer
+
+class TriageViewSet(viewsets.ModelViewSet):
+    queryset = Triage.objects.all()
+    permissions_classes = [permissions.AllowAny]
+    serializer_class = TriageSerializer
--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Eps
 from .models import Ips
 from .models import User
+from .models import PerfilUsuario
+from .models import Triage
 
 class EpsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,5 +24,17 @@ class IpsSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        #Serializa todos los campos
+        fields = '__all__'
+
+class PerfilUsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PerfilUsuario
+        #Serializa todos los campos
+        fields = '__all__' 
+
+class TriageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Triage
         #Serializa todos los campos
         fields = '__all__' 
