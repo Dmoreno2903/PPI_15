ce76e38137c6885247d2056e6394abb1e7e46b58
Cesar-580 <caospinamu@unal.edu.co>
2023-11-26 10:24:08 -0500

    Documentación de la base de datos terminada 26/11/2023

--- database/api.py
+++ database/api.py
@@ -29,29 +29,61 @@ from rest_framework.decorators import action
 
 # Se crea la vista de la relación EPS
 class EpsViewSet(viewsets.ModelViewSet):
+     """ Vista para la gestión de la entidad EPS.
+
+    Esta vista permite realizar operaciones CRUD
+    (Crear, Leer,Actualizar, Eliminar)
+    sobre los objetos EPS (Entidades Promotoras de Salud).
+
+    Atributos:
+    - `queryset`: Conjunto de objetos EPS disponibles para la vista.
+    - `permissions_classes`: Lista de clases de permisos aplicadas
+       a la vista. En este caso, se permite el acceso a cualquier 
+       usuario (permisos de AllowAny).
+    - `serializer_class`: Clase utilizada para serializar y deserializar
+       objetos EPS.
+    """
     queryset = Eps.objects.all()
     permissions_classes = [permissions.AllowAny]
     serializer_class = EpsSerializer
 
 # Se crea la vista de la relación IPS
 class IpsViewSet(viewsets.ModelViewSet):
+    """
+    Vista para la gestión de la entidad IPS, con acciones adicionales.
+
+    Atributos:
+    - `queryset`: Conjunto de objetos IPS disponibles para la vista.
+    - `permissions_classes`: Lista de clases de permisos aplicadas a la vista.
+        En este caso, se permite el acceso a cualquier usuario (permisos de AllowAny).
+    - `serializer_class`: Clase utilizada para serializar y deserializar objetos IPS.
+    """
+
     queryset = Ips.objects.all()
     permissions_classes = [permissions.AllowAny]
     serializer_class = IpsSerializer
 
     @action(detail=False, methods=['post'], url_path='filtro')
     def filter_ips(self, request):
+        """
+        Filtra y devuelve las 3 mejores opciones de IPS
+        para un usuario basándose en un cálculo ponderado
+        entre distancia y tiempo de atención.
 
-        ''' La función recibe el tirage realizado por el usuario y cálcula mediante un cálculo ponderado entre distancia
-        y tiempo de atención las 3 mejores opciones de IPS a elegir por el usuario '''
+        Parámetros:
+        - `request`: Objeto de solicitud HTTP.
 
+        Retorna:
+        - Respuesta HTTP con las 3 mejores opciones de IPS y
+        un mensaje de éxito, o un mensaje de error en caso de excepción.
+        """
         try:
             crs = 'EPSG:21897'
 
-            # Obtenemos el último trije
+            # Obtenemos el último triaje
             last_triage = pd.DataFrame(list((Triaje.objects.all()).values())).iloc[-1]
 
-            # Seleccionamos el usuario y el triage
+            # Seleccionamos el usuario y el triaje
             usuario = last_triage['user_id']
             triage = last_triage['triage_calculado']
 
@@ -68,18 +100,18 @@ class IpsViewSet(viewsets.ModelViewSet):
             })
 
             coord_usuario = gp.GeoDataFrame(
-                coord_usuario, geometry = gp.points_from_xy(coord_usuario.longitud, coord_usuario.latitud), crs='EPSG:21897'
+                coord_usuario, geometry=gp.points_from_xy(coord_usuario.longitud, coord_usuario.latitud), crs='EPSG:21897'
             )
 
             # Obtenemos todas las IPS
             df_ips = pd.DataFrame(list((Ips.objects.all()).values()))
 
-            # Aplicamos el filtro, es decir, seleccionamos únicamente las IPS cuya nivel de complejidad sea mayor o igual al triage
+            # Aplicamos el filtro, es decir, seleccionamos únicamente las IPS cuyo nivel de complejidad sea mayor o igual al triaje
             df_ips = df_ips[df_ips['complejidad'] >= triage]
 
-            # Obtenemos el geoDataFrame
+            # Obtenemos el GeoDataFrame
             geo_ips = gp.GeoDataFrame(
-                df_ips, geometry = gp.points_from_xy(df_ips.longitud, df_ips.latitud), crs='EPSG:21897'
+                df_ips, geometry=gp.points_from_xy(df_ips.longitud, df_ips.latitud), crs='EPSG:21897'
             )
 
             # Proyectamos las geometrías a un sistema de coordenadas proyectadas (por ejemplo, UTM)
@@ -90,7 +122,7 @@ class IpsViewSet(viewsets.ModelViewSet):
             geo_ips['distancia'] = geo_ips.distance(coord_usuario.iloc[0]['geometry'])
 
             # Asignamos los pesos a la distancia y al tiempo de atención
-            geo_ips['ponderado'] = ((geo_ips['tiempo_urgencias'].astype(int)) * 0.6) + (geo_ips['distancia'] * 0.4) 
+            geo_ips['ponderado'] = ((geo_ips['tiempo_urgencias'].astype(int)) * 0.6) + (geo_ips['distancia'] * 0.4)
 
             # Ordenamos y seleccionamos las 3 con el cálculo ponderado menor
             ips_validas = geo_ips.sort_values(by='ponderado').iloc[:3]
@@ -98,23 +130,46 @@ class IpsViewSet(viewsets.ModelViewSet):
             # Convertimos a un diccionario
             ips_validas_data = ips_validas.to_dict(orient='records')
 
-            # Serializamos el diccionarios
+            # Serializamos el diccionario
             ips_validas_serializer = IpsSerializer(ips_validas_data, many=True).data
-            
+
             # Devolvemos un mensaje de éxito y el objeto IPS serializado
             return Response({'message': 'Procesado correctamente', 'ips_validas': ips_validas_serializer})
         except Exception as e:
             # Devuelve un mensaje de error
-            return {'message' : str(e)}
+            return {'message': str(e)}
+
 
 
 # Se crea la vista de la relación Usuario
 class UsuarioViewSet(viewsets.ModelViewSet):
+     """ Vista para la gestión de la entidad Usuario.
+
+    Esta vista permite realizar operaciones CRUD
+    (Crear, Leer, Actualizar, Eliminar) sobre los objetos
+    Usuario en el sistema.
+
+    Atributos:
+    - `queryset`: Conjunto de objetos Usuario disponibles para la vista.
+    - `permissions_classes`: Lista de clases de permisos aplicadas a la vista.
+        En este caso, se permite el acceso a cualquier usuario
+        (permisos de AllowAny).
+    - `serializer_class`: Clase utilizada para serializar y deserializar
+        objetos Usuario.
+    """
     queryset = Usuario.objects.all()
     permissions_classes = [permissions.AllowAny]
     serializer_class = UsuarioSerializer
 
 def get_graph():
+    """
+    Genera y devuelve una representación gráfica en formato
+    base64 de la figura actual de Matplotlib.
+
+    Retorna:
+    - Cadena de caracteres en formato base64 que representa
+    la imagen gráfica en formato PNG.
+    """
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
@@ -124,64 +179,134 @@ def get_graph():
     buffer.close()
     return graph
 
+
 # Se crea la vista de la relación Triage
 class TriajeViewSet(viewsets.ModelViewSet):
+    """ Vista para la gestión de la entidad Triaje, con
+    acciones adicionales.
+
+    Atributos:
+    - `queryset`: Conjunto de objetos Triaje disponibles para la vista.
+    - `permissions_classes`: Lista de clases de permisos aplicadas a la vista.
+        En este caso, se permite el acceso a cualquier usuario
+        (permisos de AllowAny).
+    - `serializer_class`: Clase utilizada para serializar y deserializar
+        objetos Triaje.
+    """
+
     queryset = Triaje.objects.all()
     permissions_classes = [permissions.AllowAny]
     serializer_class = TriajeSerializer
+    
     @action(detail=False, methods=['get'], url_path='estadisticas_triaje')
     def get_estadisticas_triaje(self, request):
-            ''' La función calcula diferentes estadísticas '''
-            try:
-                registros = Triaje.objects.values('triage_calculado')
-                
-                # Obtiene los triages como un array de NumPy
-                triages = np.array([registro['triage_calculado'] for registro in registros])
+        """Calcula y devuelve estadísticas sobre los triajes realizados.
 
-                # Convierte los datos a numéricos utilizando NumPy
-                triages = np.array(triages, dtype=np.float64)
-                # Calcula el promedio y la desviación estándar
-                promedio = round(np.mean(triages),3)
-                desviacion_estandar = round(np.std(triages),3)
+        Parámetros:
+        - `request`: Objeto de solicitud HTTP.
 
-                return Response({'promedio': promedio, 'desviacion_estandar': desviacion_estandar})
-            except Exception as e:
-                return Response({'error': str(e)})
+        Retorna:
+        - Respuesta HTTP con las estadísticas calculadas
+        (promedio y desviación estándar).
+        """
+        try:
+            registros = Triaje.objects.values('triage_calculado')
+            
+            # Obtiene los triajes como un array de NumPy
+            triajes = np.array([registro['triage_calculado'] for registro in registros])
+
+            # Convierte los datos a numéricos utilizando NumPy
+            triajes = np.array(triajes, dtype=np.float64)
+            
+            # Calcula el promedio y la desviación estándar
+            promedio = round(np.mean(triajes), 3)
+            desviacion_estandar = round(np.std(triajes), 3)
+
+            return Response({'promedio': promedio, 'desviacion_estandar': desviacion_estandar})
+        except Exception as e:
+            return Response({'error': str(e)})
             
     @action(detail=False, methods=['get'], url_path='grafica')
     def grafica(self, request):
-            ''' La función hace una grafica de frecuencias'''
-            try:
-                registros = Triaje.objects.values('triage_calculado')
-                df = pd.DataFrame(registros)
-                # Calcula la frecuencia y crea un nuevo DataFrame
-                frecuencias = df['triage_calculado'].value_counts().reset_index()
-                frecuencias.columns = ['triage_calculado', 'frecuencia']
-                # Ordena el DataFrame en orden descendente por 'triage_calculado'
-                frecuencias = frecuencias.sort_values(by='triage_calculado')
-                #Crear el grafico con Matplotlib
-                plt.switch_backend('AGG')
-                plt.figure(figsize=(10, 5))
-                plt.title('Frecuencia de triajes')
-                plt.xlabel('Triage')
-                plt.ylabel('Frecuencia')
-                plt.bar(frecuencias['triage_calculado'],frecuencias['frecuencia'])
-                plt.xticks(frecuencias['triage_calculado'])
-                plt.tight_layout()
-                graph = get_graph()
-                return Response({'grafico': graph, "triage_calculado": frecuencias['triage_calculado'].tolist(), "frecuencia": frecuencias['frecuencia'].tolist()})
-            except Exception as e:
-                return {'message' : str(e)}
+        """Genera y devuelve una gráfica de frecuencias de los triajes.
+
+        Parámetros:
+        - `request`: Objeto de solicitud HTTP.
+
+        Retorna:
+        - Respuesta HTTP con la gráfica generada y datos de frecuencia.
+        """
+        try:
+            registros = Triaje.objects.values('triage_calculado')
+            df = pd.DataFrame(registros)
+            
+            # Calcula la frecuencia y crea un nuevo DataFrame
+            frecuencias = df['triage_calculado'].value_counts().reset_index()
+            frecuencias.columns = ['triage_calculado', 'frecuencia']
+            
+            # Ordena el DataFrame en orden ascendente por 'triage_calculado'
+            frecuencias = frecuencias.sort_values(by='triage_calculado')
+            
+            # Crea el gráfico con Matplotlib
+            plt.switch_backend('AGG')
+            plt.figure(figsize=(10, 5))
+            plt.title('Frecuencia de triajes')
+            plt.xlabel('Triage')
+            plt.ylabel('Frecuencia')
+            plt.bar(frecuencias['triage_calculado'], frecuencias['frecuencia'])
+            plt.xticks(frecuencias['triage_calculado'])
+            plt.tight_layout()
+            graph = get_graph()
+
+            return Response({
+                'grafico': graph,
+                "triage_calculado": frecuencias['triage_calculado'].tolist(),
+                "frecuencia": frecuencias['frecuencia'].tolist()
+            })
+        except Exception as e:
+            return {'message': str(e)}
+
             
     
 
-# Se crea la vista de la relación Perfil de usuario
 class PerfilViewSet(viewsets.ModelViewSet):
+    """
+    Vista para la gestión de la entidad Perfil de Usuario.
+
+    Esta vista permite realizar operaciones CRUD
+    (Crear, Leer, Actualizar, Eliminar) sobre los objetos 
+    Perfil de Usuario en el sistema.
+
+    Atributos:
+    - `queryset`: Conjunto de objetos Perfil de Usuario
+    disponibles para la vista.
+    - `permissions_classes`: Lista de clases de permisos
+    aplicadas a la vista.
+        En este caso, se permite el acceso a cualquier usuario
+        (permisos de AllowAny).
+    - `serializer_class`: Clase utilizada para serializar y
+    deserializar objetos Perfil de Usuario.
+    """
+
     queryset = PerfilUsuario.objects.all()
     permissions_classes = [permissions.AllowAny]
     serializer_class = PerfilUsuarioSerializer
 
+
 class CitasViewSet(viewsets.ModelViewSet):
+    """
+    Vista para la gestión de la entidad Citas.
+
+    Esta vista permite realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar)
+    sobre los objetos Citas en el sistema.
+
+    Atributos:
+    - `queryset`: Conjunto de objetos Citas disponibles para la vista.
+    - `permissions_classes`: Lista de clases de permisos aplicadas a la vista.
+        En este caso, se permite el acceso a cualquier usuario (permisos de AllowAny).
+    - `serializer_class`: Clase utilizada para serializar y deserializar objetos Citas.
+    """
+
     queryset = Citas.objects.all()
     permissions_classes = [permissions.AllowAny]
     serializer_class = CitasSerializer
